0d05b6ab6bd3eca98fe07805a8a26291191a19ad
wattsjon2 <wattsjon2@gmail.com>
2021-10-22 15:21:28 -0400

    updated 10/22/21 3:20

--- OOPparkinglot.py
+++ OOPparkinglot.py
@@ -1,3 +1,12 @@
+# work breakdown
+# 30 min brainstorming
+# 1 hr mesliisa coding - init, takeTicket and skeleton of def run() and made 
+# 1 hr Jon coding - view items, pay, leave
+
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
 class Garage():
     """
     The Garage Class will have capacity and items (ie cars) to place inside
@@ -21,21 +30,64 @@ class Garage():
         mark ticket paid
     """
     
-    def __init__(self, capacity = [False, False, False, False, False], tickets = {}, ticket_num = (1)):
+    def __init__(self, capacity = [False, False, False, False, False], tickets = {}, ticket_num = (1), ticket_payment_amount = {}):
         self.capacity = capacity
         self.tickets = tickets
         self.ticket_num = ticket_num
+        self.ticket_payment_amount = ticket_payment_amount
         
     def takeTicket(self):
         if False not in self.capacity:
-            print("Sorry we have not parking available today")
+            print("Sorry we have no parking available today")
         
         else:
             print(f"You are number {self.ticket_num} ")
-            self.tickets[self.ticket_num] = False
+            self.tickets[self.ticket_num] = "False"
             self.capacity.remove(False)
             self.capacity.append(True)
             self.ticket_num += 1
+
+    def viewItems(self):
+        print(self.capacity)
+        print(self.ticket_num)
+        print("Ticket Number    Paid Status")
+        for key, value in self.tickets.items():
+                print('{0:<17}{1:<4}'.format(key,value))
+        print("Ticket Number    Amount Paid")
+        for key, value in self.ticket_payment_amount.items():
+                print('{0:<17}{1:<4}'.format(key,value))
+
+    def payParking(self):
+        parking_spot = input("What is your ticket number? ")
+        if parking_spot.isdigit() == False:
+            print("Please enter a valid number")
+        elif int(parking_spot) not in self.tickets.keys():
+            print("This ticket does not exist. Please enter a valid ticket number")
+        else:
+            payment_ammount = input("What is the cost of your ticket? ")
+            try: float(payment_ammount)
+            except: 
+                print("Please enter a valid dollar amount")
+            if float(payment_ammount) < 0:
+                print("Please enter a valid dollar amount?")
+            else:
+                self.tickets[int(parking_spot)] = "True"
+                self.ticket_payment_amount[int(parking_spot)] = payment_ammount
+                print("Thank you for paying. You have 15 minutes to exit the parking garage")
+
+    def leaveGarage(self):
+        leave_number = input("Please enter your ticket number: ")
+        if leave_number.isdigit() == False:
+            print("Please enter a valid number")
+        elif int(leave_number) not in self.tickets.keys():
+            print("Please enter a valid number")
+        elif self.tickets.get(int(leave_number)) == "False":
+            print("Please pay for your ticket.")
+        else:
+            print("This ticket has been paid for. Please exit the garage and have a nice day!")
+            self.capacity.remove(True)
+            self.capacity.append(False)
+
 
     
 def run():
@@ -45,7 +97,7 @@ def run():
     while True:
         response = input("what would you like to do? park/pay/leave/quit: ")
         
-        if response.lower() not in ['park','pay','leave','quit']: #membership check
+        if response.lower() not in ['park','pay','leave','quit','view']: #membership check
             response = input('Please enter a VALID choice park/pay/leave or quit:')     
         
         if response.lower() == 'quit':
@@ -54,11 +106,14 @@ def run():
         
         elif response.lower() == 'park':
             jonlissa.takeTicket()
-            
-        # elif response.lower() == 'pay':
-        #     # mage_bag.addToInventory()
 
-        # elif response.lower() == 'leave':
-        #     # mage_bag.showInventory()
+        elif response.lower() == "view":
+            jonlissa.viewItems()
+            
+        elif response.lower() == 'pay':
+            jonlissa.payParking()
+
+        elif response.lower() == 'leave':
+            jonlissa.leaveGarage()
 
 run()
